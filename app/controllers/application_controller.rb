095a6919e6b423a2a711c8d23101d62598523670
Guillermo Martinez <dumblemoe@gmail.com>
2020-10-14 12:41:58 -0500

    Building full matches controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
     def home
+        @user = current_user ? current_user.email.split("@")[0]:nil
     end
-
 end
---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,56 @@
+class MatchesController < ApplicationController
+    before_action :find_match, only:[:show, :edit, :update, :destroy]
+    #show /show all page
+    def index
+        @match = Match.all
+    end
+
+    #Get /read action for a single object
+    def show 
+    end         
+
+    #Post /create action
+    def create 
+        @match = Match.new(match_params)
+        if @match.valid?
+            @match.save
+            redirect_to match_path(@match.id)
+        else
+            @errors = @match.errors.full_messages
+            render :new
+        end
+    end
+
+    #Get /render new form
+    def new
+        @match = Match.new
+        4.times{@match.players.build}
+    end
+
+    #Get /edit form 
+    def edit
+    end
+
+    #Post /update action
+    def update
+        @match.update(params.require(:match).permit(:match_number, :match_date))
+        redirect_to match_path(@match.id)    
+    end 
+
+    #Post /destroy action
+    def destroy 
+        # byebug
+        @match.destroy
+        redirect_to matches_path
+    end
+
+    private
+    def match_params
+        params.require(:match).permit(:match_number, :match_date, players_attributes:[:id, :name, :player_number])
+    end
+
+    def find_match
+        @match = Match.find(params[:id])
+    end
+    
+end
